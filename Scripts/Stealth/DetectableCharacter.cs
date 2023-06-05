c568f2bd2949edce02046918f6bff299a4a7d205
Dameon Laird <dameonlair@gmail.com>
2023-06-04 20:39:56 -0700

    Added normalization for lighting levels

--- Scripts/Stealth/DetectableCharacter.cs
+++ Scripts/Stealth/DetectableCharacter.cs
@@ -12,9 +12,19 @@ namespace GreenMambaStealth.Stealth
 		[SerializeField]
 		private List<LightDetector> _lightDetectors = new List<LightDetector>();
 
-		[Tooltip("How much to multiply the character's stealth rating by. Default of 0.5 is best for most lighting.\nTurn this up in dark environments, or down in bright environments.")]
+		[Tooltip("How much to multiply the character's stealth rating by. \nTurn this up in dark environments, or down in bright environments.")]
 		[SerializeField]
-		private float _stealthMultiplier = 0.5f;
+		private float _stealthMultiplier = 1;
+
+		[SerializeField]
+		private float _normalMin = 0;
+		[SerializeField]
+		private float _normalMax = 1;
+
+		private void Update()
+		{
+			Debug.Log(Visibility);
+		}
 
 		/// <summary>
 		/// A float in the range of (0.01, 1) that represents the visibility of the character.
@@ -24,8 +34,14 @@ namespace GreenMambaStealth.Stealth
 			get
 			{
 				float stealthRating = (_lightDetectors.Count > 0) ? AverageLightLevel() : 1;
+				if (stealthRating <= _normalMin)
+				{
+					return 0;
+				}
 
-				return stealthRating * _stealthMultiplier;
+				float normalRange = _normalMax - _normalMin;
+				float normalizedRating = Mathf.Clamp01(((stealthRating - _normalMin) * _stealthMultiplier) / normalRange);
+				return normalizedRating;
 			}
 		}
 
